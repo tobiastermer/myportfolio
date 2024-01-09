1544b8e696399e5bb898aa4e0bff028b126073c9
tobiastermer <tobiastermer@gmx.de>
2024-01-09 21:42:41 +0100

    about me section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../shared/footer/footer.component';
-import { HeaderComponent } from '../shared/header/header.component';
 import { LandingComponent } from './landing/landing.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -10,7 +8,7 @@ import { ContactComponent } from './contact/contact.component';
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, LandingComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent],
+  imports: [LandingComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
