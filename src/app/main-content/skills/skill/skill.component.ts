45c7fe86dd1bff30ffefd10f15da88406841661f
tobiastermer <tobiastermer@gmx.de>
2024-01-09 23:56:26 +0100

    my skills section

---
+++ src/app/main-content/skills/skill/skill.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  standalone: true,
+  imports: [],
+  templateUrl: './skill.component.html',
+  styleUrl: './skill.component.scss'
+})
+export class SkillComponent {
+  @Input() skill?: string;
+}
--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { SkillComponent } from './skill/skill.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [SkillComponent, CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills: string[] = [
+    'Angular',
+    'TypeScript',
+    'JavaScript',
+    'HTML',
+    'Firebase',
+    'GIT',
+    'CSS',
+    'Rest-Api',
+    'Scrum',
+    'Material Design',
+  ]
 }
