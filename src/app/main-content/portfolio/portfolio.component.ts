aa7869333a58cad63c6d78d398537ff496632a90
tobiastermer <tobiastermer@gmx.de>
2024-01-10 19:53:39 +0100

    portfolio structure implemented

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [ProjectComponent, CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+
+  portfolio: any = [
+    {
+      projectname: 'Join',
+      languages: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      linkLive: 'https://tobias-termer.developerakademie.net/join/index.html',
+      linkGit: 'https://github.com/tobiastermer/join',
+      picture: 'join.png',
+    },
+    {
+      projectname: 'Join',
+      languages: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      linkLive: 'https://tobias-termer.developerakademie.net/join/index.html',
+      linkGit: 'https://github.com/tobiastermer/join',
+      picture: 'join.png',
+    },
+    {
+      projectname: 'Join',
+      languages: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      linkLive: 'https://tobias-termer.developerakademie.net/join/index.html',
+      linkGit: 'https://github.com/tobiastermer/join',
+      picture: 'join.png',
+    },
+    {
+      projectname: 'Join',
+      languages: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      linkLive: 'https://tobias-termer.developerakademie.net/join/index.html',
+      linkGit: 'https://github.com/tobiastermer/join',
+      picture: 'join.png',
+    },
+  ]
 
 }
--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
-
+  @Input() projectname?: string;
+  @Input() languages?: string;
+  @Input() description?: string;
+  @Input() linkLive?: string;
+  @Input() linkGit?: string;
+  @Input() picture?: string;
+  @Input() index?: number;
 }
