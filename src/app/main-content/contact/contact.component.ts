62f39ec8cc9fcefbcf313dbf281353c38d356b6d
tobiastermer <tobiastermer@gmx.de>
2024-01-14 12:36:43 +0100

    contact formular and footer completed

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,50 +1,82 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, CommonModule], // FormsModule hinzugefügt
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  @ViewChild('form') form!: NgForm;
+  formSubmittedSuccessfully = false; // Neuer Status für Erfolgsmeldung
 
-  @ViewChild('form') form!: ElementRef;
-  @ViewChild('formName') formName!: ElementRef;
-  @ViewChild('formEmail') formEmail!: ElementRef;
-  @ViewChild('formMessage') formMessage!: ElementRef;
-  @ViewChild('formButton') formButton!: ElementRef;
+  areAllFieldsEmpty(): boolean {
+    if (!this.form || !this.form.value) {
+      return true;
+    }
+    return !this.form.value.name && !this.form.value.email && !this.form.value.message;
+  }
+
+  isValidEmail(email: string): boolean {
+    // Regulärer Ausdruck für eine strenge E-Mail-Validierung
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(email);
+  }
 
   async sendMail() {
-    // action https://f0164f24@w01eabb4.kasserver.com/assets/server/send_mail.php
-    console.log("Sending mail");
-    let formName = this.formName.nativeElement;
-    let formEmail = this.formEmail.nativeElement;
-    let formMessage = this.formMessage.nativeElement;
-    let formButton = this.formButton.nativeElement;
-    formName.disabled = true;
-    formEmail.disabled = true;
-    formMessage.disabled = true;
-    formButton.disabled = true;
+    if (this.form.invalid) {
+      // Benutzerbenachrichtigung hinzufügen, falls erforderlich
+      return;
+    }
 
+    this.toggleFormDisabled(true);
+    const formData = this.prepareFormData();
+
+    try {
+      await this.performMailSend(formData);
+    } catch (error) {
+      console.error('Fehler beim Senden der E - Mail: ', error);
+      // Fehlermeldung hinzufügen
+    } finally {
+      this.toggleFormDisabled(false);
+    }
+  }
+
+  prepareFormData(): FormData {
+    const { name, email, message } = this.form.value;
     let fd = new FormData();
-    fd.append('name', formName.value);
-    fd.append('email', formEmail.value);
-    fd.append('message', formMessage.value);
+    fd.append('name', name);
+    fd.append('email', email);
+    fd.append('message', message);
+    return fd;
+  }
 
-    await fetch('https://www.portfolio.tobias-termer.de/assets/server/send_mail.php',
-    
-      {
-        method: 'POST',
-        body: fd
-      });
+  async performMailSend(formData: FormData) {
+    const response = await fetch('https://www.portfolio.tobias-termer.de/assets/server/send_mail.php', {
+      method: 'POST',
+      body: formData
+    });
+  
+    if (!response.ok) {
+      throw new Error('Netzwerkantwort war nicht ok.');
+    }
+  
+    // Formular zurücksetzen und Erfolgsmeldung anzeigen
+    this.form.reset();
+    this.formSubmittedSuccessfully = true;
+    setTimeout(() => this.formSubmittedSuccessfully = false, 3000); // Meldung nach 3 Sekunden ausblenden
+  }
 
-    // Text Nachricht gesendet
-
-    formName.disabled = false;
-    formEmail.disabled = false;
-    formMessage.disabled = false;
-    formButton.disabled = false;
+  toggleFormDisabled(disabled: boolean) {
+    Object.keys(this.form.controls).forEach(key => {
+      if (disabled) {
+        this.form.controls[key].disable();
+      } else {
+        this.form.controls[key].enable();
+      }
+    });
   }
 }
